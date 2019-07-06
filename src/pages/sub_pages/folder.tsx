9eacdd8f6f2019218bd9d305031089cdee63c171
Hyunsub Kim <embrapers263@gmail.com>
2019-07-06 15:16:57 +0900

    redirect file to raw url

--- src/pages/sub_pages/folder.tsx
+++ src/pages/sub_pages/folder.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { extname } from 'path';
 import { ServerResponse, File } from '../../models';
 
 export default function FolderPage (res: ServerResponse) {
   const files: File[] = res.payload as File[];
-  const fileCompList = files.map((file: File) => (
-    <tr key={ file.name }>
-      <td><a href={ `${file.path}` }>{ file.name + ((file.isDir) ? '/' : '') }</a></td>
-      <td>{ file.size || '-' }</td>
-    </tr>  
-  ));
+  const fileCompList = files.map((file: File) => {
+    const ext = extname(file.path);
+    const filePath = (ext === '') ? file.path : 'http://home.hyunsub.kim:8080' + file.path + "?raw";
     
+    return (
+      <tr key={ file.name }>
+        <td><a href={ `${filePath}` }>{ file.name + ((file.isDir) ? '/' : '') }</a></td>
+        <td>{ file.size || '-' }</td>
+      </tr>  
+    )
+  });
+  
   return (
     <table>
       <thead>
