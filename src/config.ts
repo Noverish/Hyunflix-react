4509395bcdfef631916a3302b24f55e9b0f9630d
Hyunsub Kim <embrapers263@gmail.com>
2019-10-23 09:25:12 +0000

    Server path from current hostname

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,12 @@ export const MOBILE_BREAKPOINT = 768;
 export const USER_INPUT_DEBOUNCE = 500;
 export const COLORS = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple'];
 
-export const SOCKET_SERVER = 'http://home.hyunsub.kim:8080';
+export const SOCKET_SERVER = `http://${window.location.hostname}:8080`;
 export const FFMPEG_SOCKET_PATH = '/fs/socket.io/ffmpeg';
 export const YOUTUBE_SOCKET_PATH = '/fs/socket.io/youtube';
 export const USER_VIDEO_SOCKET_PATH = '/api/socket.io/user/video';
 export const VIDEO_EXAMINE_SOCKET_PATH = '/api/socket.io/videos/examine';
 
-export const API_SERVER = 'http://home.hyunsub.kim:8080/api';
-export const FS_SERVER = 'http://home.hyunsub.kim:8080/fs';
-export const AUTH_SERVER = 'http://home.hyunsub.kim:8080/auth';
+export const API_SERVER = `http://${window.location.hostname}:8080/api`;
+export const FS_SERVER = `http://${window.location.hostname}:8080/fs`;
+export const AUTH_SERVER = `http://${window.location.hostname}:8080/auth`;
