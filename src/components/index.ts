1710c6d32ca5817def8b30e7ea3741cba706197e
Hyunsub Kim <embrapers263@gmail.com>
2019-10-31 11:40:09 +0000

    Deleted unnecessary components

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,17 +5,12 @@ export { default as VideoBundleList } from './video/video-bundle-list';
 export { default as VideoBundleItem } from './video/video-bundle-item';
 export { default as VideoPlayer } from './video/video-player';
 
-export { default as MusicEditModal } from './music/music-edit-modal';
 export { default as MusicPlayModal } from './music/music-play-modal';
 export { default as MusicPlayList } from './music/music-play-list';
 export { default as MusicPlayer } from './music/music-player';
 export { default as MusicPlayControl } from './music/music-play-control';
 export { default as MusicArticleList } from './music/music-article-list';
 export { default as MusicArticleItem } from './music/music-article-item';
-
-export { default as FileSelectModal } from './etc/file-select-modal';
-export { default as FileSelectList } from './etc/file-select-list';
-export { default as FileSelectItem } from './etc/file-select-item';
 
 export { default as UserVideoList } from './user/user-video-list';
 export { default as UserAvatarMenu } from './user/user-avatar-menu';
