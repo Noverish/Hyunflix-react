c3f057b80543381f6881135aba141df6b566f252
Hyunsub Kim <embrapers263@gmail.com>
2019-11-10 07:42:52 +0000

    Added go to video series button

--- src/pages/video/video-list.tsx
+++ src/pages/video/video-list.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
 
 import withContainer from 'components/hoc/container';
 import { VideoList } from 'components';
@@ -9,12 +11,19 @@ const VideoListContainer = withContainer<Video>()(VideoList);
 const link = (video: Video) => `/videos/${video.id}`;
 
 const VideoListPage: React.FunctionComponent = () => {
+  const headerExtra = (
+    <Link to="/videos/series">
+      <Button type="primary">시리즈 별로 보기</Button>
+    </Link>
+  );
+
   return (
     <VideoListContainer
       title="Video"
       subTitle="영화, 드라마, 예능"
       search={videoList}
       link={link}
+      headerExtra={headerExtra}
     />
   );
 };
